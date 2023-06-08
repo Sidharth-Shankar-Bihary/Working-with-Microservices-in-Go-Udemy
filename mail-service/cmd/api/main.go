72701214b8c80eecc68f2949b924c25d92852bc6
Sidharth-Shankar-Bihary <sidharth.bihary@gmail.com>
2023-06-08 22:39:15 +0530

    mailer service

---
+++ mail-service/cmd/api/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+type Config struct {
+	Mailer Mail
+}
+
+const webPort = "80"
+
+func main() {
+	app := Config{
+		Mailer: createMail(),
+	}
+	log.Println("Starting mail servcie on port ", webPort)
+
+	//starting the server
+	app.serve()
+}
+
+func (app *Config) serve() {
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+	log.Println("Starting service on port ", webPort)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+func createMail() Mail {
+	port, _ := strconv.Atoi(os.Getenv("MAIL_PORT"))
+	m := Mail{
+		Domain:      os.Getenv("MAIL_DOMAIN"),
+		Host:        os.Getenv("MAIL_HOST"),
+		Port:        port,
+		Username:    os.Getenv("MAIL_USERNAME"),
+		Password:    os.Getenv("MAIL_PASSWORD"),
+		Encryption:  os.Getenv("MAIL_ENCRYPTION"),
+		FromName:    os.Getenv("FROM_NAME"),
+		FromAddress: os.Getenv("FROM_ADDRESS"),
+	}
+	return m
+}
---
+++ mail-service/go.mod
@@ -0,0 +1,10 @@
+module mailer-service
+
+go 1.16
+
+require (
+	github.com/go-chi/chi/v5 v5.0.8 // indirect
+	github.com/go-chi/cors v1.2.1 // indirect
+	github.com/vanng822/go-premailer v1.20.2 // indirect
+	github.com/xhit/go-simple-mail/v2 v2.13.0 // indirect
+)
